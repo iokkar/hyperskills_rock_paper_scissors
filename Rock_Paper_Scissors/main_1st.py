1b0b7fc36228e05a1fd08ace46c377866bea03bb
Dmitriy Klimentew <m4me@iMac-Dmitriy-Klimentew.local>
2020-07-25 08:22:31 +0300

    Second stage.

---
+++ Rock_Paper_Scissors/main_1st.py
@@ -0,0 +1,17 @@
+# 1 этап 24/07
+
+pl_choice = input()
+option = ''
+
+def check_win():
+    global option
+    if pl_choice == 'rock':
+        option = 'paper'
+    elif pl_choice == 'scissors':
+        option = 'rock'
+    elif pl_choice == 'paper':
+        option = 'scissors'
+
+
+check_win()
+print(f'Sorry, but computer chose {option}')
