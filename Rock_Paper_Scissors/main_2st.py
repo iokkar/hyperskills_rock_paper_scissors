99763b8804b37756e33ffb4183c0fb46ab05d7fb
Dmitriy Klimentew <m4me@iMac-Dmitriy-Klimentew.local>
2020-07-27 09:00:01 +0300

    Первый вариант второго этапа

--- Rock_Paper_Scissors/main_2st.py
+++ Rock_Paper_Scissors/main_2st.py
@@ -1,17 +1,23 @@
-# 2 этап 24/07
+# 2 этап 27/07
 
-pl_choice = input()
-option = ''
+#Сегодня hyperskill лежит, написал задание по памяти, возможно не точно
+#в таком виде работае, нужно уточнять 
+# задание и условия когда сайт поднимут
 
-def check_win():
-    global option
-    if pl_choice == 'rock':
-        option = 'paper'
-    elif pl_choice == 'scissors':
-        option = 'rock'
-    elif pl_choice == 'paper':
-        option = 'scissors'
+import random
 
+comp_choose = ('paper','rock','scissors')
+cmp_ch = random.choice(comp_choose)
+win = dict(rock='scissors', scissors='paper',paper='rock')
+pl_ch = input() #'scissors'
 
-check_win()
-print(f'Sorry, but computer chose {option}')
+def state():
+    if pl_ch == cmp_ch:
+        print("Draw")
+    elif win[pl_ch] == cmp_ch:
+        print('Player win')
+    elif win[cmp_ch] == pl_ch:
+        print('Computer win')
+
+state()
+    
