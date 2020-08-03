e882708b40ea7b98efc8e0e8c02264f53650b290
Dmitriy Klimentew <m4me@iMac-Dmitriy-Klimentew.local>
2020-08-03 08:56:50 +0300

    Добавлено считывание из файла в словарь

--- Rock_Paper_Scissors/develop.py
+++ Rock_Paper_Scissors/develop.py
@@ -1,27 +1,34 @@
 # development in progress 
-# 01/08
+# 03/08
 # добавлено поиск в файле и вывод очков
 # добавляет нового польльзователя, но выводит неправильно, нужна проверка
+# добавил считывание из файла в словарь
+# провека и добавление нового игрока в словарь, а не в файл
 
 # нужно добавить:
-# добавление дового игрока в файл
+# добавление нового игрока в файл
+# не нужно добавлять нового игрока в файл, пока добавляем его в словарь с игроками!!!
 # изменение очков для игрока после каждого раунда игры
 
-# (по умолчанию выводится до 3 цифр, но возможно нужно сделать проверку
-# через len, до пробела посчитать кол-во симовол и только потом распечатать)
 
+# проверка добавленных игроков в словаре
+# 
 
 pl_ch = ''
 pl_name = input('Enter your name: ')
 print(f'Hello {pl_name}')
+pl_names = {}
 
+
+# есть проблема с добавлением 
 def check_name(): # проверка имени перебором
     f_names = open("rating.txt","a+")
     for i in f_names:
         if i.startswith(pl_name):
             f_names.close()
             return
-    f_names.write('\n'+ pl_name + ' 0')
+    #f_names.write('\n'+ pl_name + ' 0') - запись имени в файл
+    pl_names[pl_name] = 0
     f_names.close()
     return
 
@@ -31,7 +38,13 @@ def ch_name(): # короткая версия
         f_names.write('\n'+ pl_name + ' 0')
     f_names.close()
 
-def find_rating():
+def f_rating(): # поиск в словаре
+    if pl_name in pl_names:
+        print(pl_name, pl_names[pl_name])
+        return
+
+
+def find_rating(): #поиск в файле
     f_names = open("rating.txt","r+")
     for name in f_names:
         if pl_name in name:
@@ -40,13 +53,23 @@ def find_rating():
             break
     f_names.close()
 
-ch_name()
+def read_names(): # считываю имена из файла в словарь  - работате
+    f_names = open("rating.txt","r+")
+    for i in f_names:
+            pl_names[i[:i.index(' ')]] = int(i[i.index(' ')+1:])
+    f_names.close()
+
+read_names()
+check_name()
 while pl_ch != '!exit':
     pl_ch = input()
     if pl_ch == '!rating':
-        find_rating()
+        f_rating()
+        #find_rating()
     #check_in()
     #state()
 else:
+    for i in pl_names: # тест 
+        print(i, pl_names[i])
     print('Bye!')
 
--- Rock_Paper_Scissors/training_1.py
+++ Rock_Paper_Scissors/training_1.py
@@ -39,27 +39,17 @@ check_name()
 check()
 
 # срезы
+# 03.08
+# считывание строк и добавление их в словарь
 
 str = ('Tim 500','Dim 0')
 name = 'Nina'
 names = {}
-print(str[0][:str[0].rfind(' ')])
-print(str[0][str[0].index(' ')+1:])
-b = int(str[0][str[0].index(' ')+1:])
-a = str[0][:str[0].rfind(' ')+1].replace(' ','')
-#for i in str:
- #   names[i[]]
-names[a] = b
-
-
-str = ('Tim 500','Dim 0')
-name = 'Nina'
-names = {}
-#print(str[0][:str[0].rfind(' ')])
-#print(str[0][str[0].index(' ')+1:])
-#b = int(str[0][str[0].index(' ')+1:])
-#a = str[0][:str[0].rfind(' ')+1].replace(' ','')
+a = ''
+b = 0
 for i in str:
     print(i[:i.index(' ')])
+    a = i[:i.index(' ')]
     print(i[i.index(' ')+1:])
-#names[a] = b
+    b = i[i.index(' ')+1:]
+    names[i[:i.index(' ')]] = int(i[i.index(' ')+1:])
